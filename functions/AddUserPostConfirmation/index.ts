951b651b1fff225fc2de86d21b00ca3b37eba6fc
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-05-20 19:54:17 -0500

    feat(auth): add user post confirmation lambda code

--- functions/AddUserPostConfirmation/index.ts
+++ functions/AddUserPostConfirmation/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello")
+import { DynamoDBClient, PutItemCommand} from "@aws-sdk/client-dynamodb";
+import { 
+    PostConfirmationConfirmSignUpTriggerEvent 
+} from "aws-lambda";
+
+import { marshall } from "@aws-sdk/util-dynamodb";
+
+const client = new DynamoDBClient({
+    region: "us-east-1"
+});
+
+exports.handler = async function(
+    event: PostConfirmationConfirmSignUpTriggerEvent,
+): Promise<PostConfirmationConfirmSignUpTriggerEvent> {
+    
+    const date = new Date();
+    const isoDate = date.toISOString();
+
+    const params = {
+        TableName: "Users",
+        Item: marshall({
+            UserID: event.request.userAttributes.sub,
+            Email: event.request.userAttributes.email,
+            Name: event.request.userAttributes.name,
+            CreatedAt: isoDate,
+            __typename: "User"
+        })
+    }
+
+    try {
+        await client.send(new PutItemCommand(params));
+    } catch (error: any) {
+        console.log(error);
+    }
+
+    return event;
+}
