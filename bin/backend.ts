0489e2d7f7ee026e4df6e50a19bd36adf3975c64
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-05-21 11:43:33 -0500

    feat(auth): import auth-stack to backend entry point

--- bin/backend.ts
+++ bin/backend.ts
@@ -3,11 +3,16 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DatabaseStack } from '../stacks/database-stack';
 import { ComputeStack } from '../stacks/compute-stack';
+import { AuthStack } from '../stacks/auth-stack';
 
 const app = new cdk.App();
 const databaseStack = new DatabaseStack(app, "DatabaseStack");
 
 const computeStack = new ComputeStack(app, "ComputeStack", {
   usersTable: databaseStack.usersTable
+});
+
+const authStack = new AuthStack(app, "AuthStack", {
+  addUserPostConfirmation: computeStack.addUserToTableFunc
 });
 
