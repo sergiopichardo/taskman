3aa609a9ccf8e2980b703eb0868f48db311428e9
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-05-23 15:39:29 -0500

    feat(database): add TodosTable and global secondary index

--- stacks/database-stack.ts
+++ stacks/database-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from "constructs";
 
 export class DatabaseStack extends cdk.Stack {
     public readonly usersTable: Table;
+    public readonly todosTable: Table;
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props); 
@@ -15,6 +16,34 @@ export class DatabaseStack extends cdk.Stack {
             },
             billingMode: BillingMode.PAY_PER_REQUEST,
             removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+
+        this.todosTable = new Table(this, "TodosTable", {
+            tableName: "Todos",
+            partitionKey: {
+                name: "UserID",
+                type: AttributeType.STRING,
+            },
+            sortKey: {
+                name: "TodoID",
+                type: AttributeType.STRING
+            },
+            billingMode: BillingMode.PAY_PER_REQUEST,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        })
+
+        // Allows you to create different partition key to create data 
+        this.todosTable.addGlobalSecondaryIndex({
+            indexName: "getTodoId",
+            partitionKey: {
+                name: "UserID",
+                type: AttributeType.STRING
+            },
+            // This is temporary (not a good way to fetch data)
+            sortKey: {
+                name: "title",
+                type: AttributeType.STRING
+            }
         })
     }
 }
