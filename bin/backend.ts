8beeab372cdf2784f290c507d05d3b72ef5b2f7b
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-05-25 13:32:38 -0500

    feat(appSync): add AppSync stack to backend file

--- bin/backend.ts
+++ bin/backend.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { DatabaseStack } from '../stacks/database-stack';
 import { ComputeStack } from '../stacks/compute-stack';
 import { AuthStack } from '../stacks/auth-stack';
+import { AppSyncStack } from '../stacks/appsync-stack';
 
 const app = new cdk.App();
 const databaseStack = new DatabaseStack(app, "DatabaseStack");
@@ -16,3 +17,6 @@ const authStack = new AuthStack(app, "AuthStack", {
   addUserPostConfirmation: computeStack.addUserToTableFunc
 });
 
+const appSyncStack = new AppSyncStack(app, "AppSyncStack", {
+  userPool: authStack.todoUserPool
+})
